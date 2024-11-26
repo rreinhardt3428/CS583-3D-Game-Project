8813eed71afef1536cc59bd2bf766395d807cf82
Enoc Gitty <enocrodriguez077@gmail.com>
2024-11-25 21:47:24 -0800

    Added shooting mechanic that points towards an added crosshair, using a test gun and bullet that can be changed later.

---
+++ 3D Game Project/Assets/Scripts/Shooting.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun1Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform gunPoint;
+    public Cinemachine.CinemachineVirtualCamera thirdPersonCamera;
+    public float bulletForce = 20f;
+    public float fireRate = 0.5f;
+    public int damage = 10; // can be changed for each gun in inspector 
+    private float timer = 0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= timer)
+        {
+            timer = Time.time + fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Transform cameraTransform = thirdPersonCamera.VirtualCameraGameObject.transform;
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+        RaycastHit hit;
+        Vector3 shootDirection;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            shootDirection = (hit.point - gunPoint.position).normalized;
+        }
+        else
+        {
+            Vector3 targetPoint = cameraTransform.position + cameraTransform.forward * 1000f;
+            //shootDirection = cameraTransform.forward;
+            shootDirection = (targetPoint - gunPoint.position).normalized;
+        }
+        
+        //GameObject bullet = Instantiate(bulletPrefab, gunPoint.position, Quaternion.identity); 
+        //bullet.transform.forward = shootDirection;
+        Quaternion bulletRotation = Quaternion.LookRotation(shootDirection) * Quaternion.Euler(90, 0, 0);
+        GameObject bullet = Instantiate(bulletPrefab, gunPoint.position, bulletRotation);
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+
+        if (bulletRb != null)
+        {
+            bulletRb.AddForce(shootDirection * bulletForce, ForceMode.Impulse);
+        }
+
+        Destroy(bullet, 3f);
+    }
+}
