f9e7ce9a28218818b35e83fa0986604e543839dd
R-Albon3 <126095069+R-Albon3@users.noreply.github.com>
2024-12-06 19:01:19 -0800

    Sprint + Jump & Camera Fixes

--- 3D Game Project/Assets/Scripts/PlayerMovement.cs
+++ 3D Game Project/Assets/Scripts/PlayerMovement.cs
@@ -17,25 +17,57 @@ public class PlayerMovement : MonoBehaviour
    //Set gravity and velocity for the player
    public float gravity = -20.0f;
    Vector3 velocity;
+   //Set jump force
+   public float jumpForce = 10.0f;
+   //Set sprint speed
+   public float sprintSpeed;
+   //Placeholder speed
+   float baseSpeed;
 
 
    void Start()
    {
    //Set the controller variable to the relevant component
    playerController = GetComponent<CharacterController>();
+   //Grab the base speed of the player
+   baseSpeed = playerSpeed;
    }
 
    void Update()
    {
    //Grab current inputs from the player
-   horizontalInput = Input.GetAxis("Horizontal");
-   verticalInput = Input.GetAxis("Vertical");
+   horizontalInput = Input.GetAxisRaw("Horizontal");
+   verticalInput = Input.GetAxisRaw("Vertical");
+   //Checks to see if the player is moving forward and pressing LeftShift
+   if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+   {
+      //Makes sure the player isnt moving side to side
+      if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+      {
+            //Changes playerSpeed to sprintSpeed for direction calculation
+            playerSpeed = sprintSpeed;
+      } else {
+            playerSpeed = baseSpeed;
+      }
+   } else {
+      playerSpeed = baseSpeed;
+   }
    //Sets player direction based on inputs
-   Vector3 playerDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+   Vector3 playerDirection = (transform.forward * verticalInput + transform.right * horizontalInput).normalized;
    //Moves the player based on direction and speed
    playerController.Move(playerDirection * playerSpeed * Time.deltaTime);
    //Sets the sphere position based on character
    spherePosition = new Vector3(transform.position.x, transform.position.y - floorOffset, transform.position.z);
+   //Input used for jumping which adds a y velocity if the player presses the corresponding button
+   if(Input.GetKeyDown(KeyCode.Space))
+   {
+      //Player can only jump if theyre currently grounded
+      if(Physics.CheckSphere(spherePosition, playerController.radius - 0.05f, floorMask))
+      {
+            velocity.y += jumpForce;
+      } 
+   }
+
    //Checks to see whether the sphere is colliding with the floor and if not applies gravity / velocity to the player
    if(!Physics.CheckSphere(spherePosition, playerController.radius - 0.05f, floorMask))
    {
@@ -46,4 +78,6 @@ public class PlayerMovement : MonoBehaviour
    //Moves player towards the floor if in the air
    playerController.Move(velocity * Time.deltaTime);
    }
+
+   
 }
