9ac00be57d17315f11a74be9c4f0f76015477d8f
Enoc Gitty <enocrodriguez077@gmail.com>
2024-12-08 22:54:52 -0800

    Added gun powerup, unlimited ammo for 5 seconds after pick up, used test cube with script, added bullet trails to hitscan for better shooting visuals with some adjustments to the script, changed impact effect color. Added healthbar and healthbar script, press enter to take damage and after 5 seconds without damage you will be auto healed. PlayerHealth and HealthPowerup to be implemented later when other enemies that can damage player are added (or just at a later date). Added 2D package for health bar. Readded the crosshair after unity closed suddenly and a few things were gone (only like 2 or 3 small things from the hierarchy tab).

---
+++ 3D Game Project/Assets/Scripts/GunPowerup.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPowerup : MonoBehaviour
+{
+    public float frMulti = 0.5f;
+    public float duration = 5f;
+    private Renderer powerupRenderer;
+    private Collider powerupCollider;
+
+    void Start()
+    {
+        powerupRenderer = GetComponent<Renderer>();
+        powerupCollider = GetComponent<Collider>();
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // this specifically searching on the camera because i have the gun attached to it
+            // this would need to be edited in the future for the specific model which i can do whenever
+            Cinemachine.CinemachineVirtualCamera camera = FindObjectOfType<Cinemachine.CinemachineVirtualCamera>();
+            Gun1Shooting gun = camera.GetComponentInChildren<Gun1Shooting>();
+            StartCoroutine(PowerUp(gun));
+            powerupRenderer.enabled = false;
+            powerupCollider.enabled = false;
+        }
+    }
+
+    IEnumerator PowerUp(Gun1Shooting gun)
+    {
+        Debug.Log("Fire rate power up on");
+        float currFireRate = gun.fireRate;
+        bool unlimited = gun.unlimitedAmmo;
+
+        gun.fireRate *= frMulti;
+        gun.unlimitedAmmo = true;
+
+        yield return new WaitForSeconds(duration);
+
+        gun.fireRate = currFireRate;
+        gun.unlimitedAmmo = unlimited;
+
+        Debug.Log("Power up off.");
+        Destroy(gameObject);
+    }
+}
